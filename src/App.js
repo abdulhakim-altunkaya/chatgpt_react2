eaeb671aae1bff539d67c0b600c8443139c0277a
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-02-07 17:25:37 +0300

    project first version is finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
 
 function App() {
+  let[promptInput, setPromptInput] = useState("");
+  let[displayText, setDisplayText] = useState("");
 
+  const apiapi = process.env.REACT_APP_CHATGPT_API;
+  const configuration = new Configuration({
+      apiKey: apiapi
+  })
+  const openai = new OpenAIApi(configuration);
+  
+  const sendMessage = async () => {
+    const response = await openai.createCompletion({
+      prompt: promptInput,
+      model: "text-davinci-003",
+      temperature: 1,
+      max_tokens: 1000
+    });
+    setDisplayText(response.data.choices[0].text)
+  }
   
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    sendMessage();
+  }
+  
+  
   return (
     <div className="App">
-      <p>hello {process.env.REACT_APP_CHATGPT_API}</p>
-      
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="promptInput"
+            value={promptInput} onChange={(e) => setPromptInput(e.target.value)}/>
+            <button type="submit">SUBMIT</button>
+        </form>
+        <div>{displayText}</div>
     </div>
   );
 }
