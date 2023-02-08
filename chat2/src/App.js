32fc98a4e818fdca5308cbdaee925cef27ccdfec
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-02-08 11:45:41 +0300

    first version is completed

--- chat2/src/App.js
+++ chat2/src/App.js
@@ -1,10 +1,42 @@
 import { Configuration, OpenAIApi } from "openai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [prompt, setPrompt] = useState("");
+  let [response, setResponse] = useState("hello");
+
+  const api = process.env.REACT_APP_CHATGPT_API;
+  const configuration = new Configuration({
+    apiKey: api
+  });
+  const openai = new OpenAIApi(configuration);
+  const sendMessage = async () => {
+    const response = await openai.createCompletion({
+      prompt: "how are you?",
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 1000,
+    })
+    const message = response.data.choices[0].text
+    setResponse(message);
+  }
+
+  useEffect(() => {
+    sendMessage();
+  }, [])
+  
+
   return (
     <div className="App">
-      <p>work test</p>
+      <form>
+        <textarea name="prompt" 
+        id="prompt" 
+        cols="30" rows="10"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}></textarea>
+        <button>SUBMIT</button>
+      </form>
+      <div>{response}</div>
     </div>
   );
 }
